55d19193f849fbc0ad0a3276e8a6b1835abea1bd
Kersul <joaohkfaria@gmail.com>
2019-12-19 21:02:27 -0300

    Adds cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 import spotifyRouter from './routes/spotify';
@@ -40,6 +41,7 @@ MongooseService.connection.on('connected', () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
